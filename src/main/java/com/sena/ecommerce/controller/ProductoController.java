1bfa9d4a2d45ab79f85621ffd521cfeb41109d59
sharid <arguellosharid19@gmail.com>
2025-08-22 14:07:19 -0500

    v0.7_vistaShowCompletaUpdateTemplateAdminUpdateClaseProductoController_sharid_22082025

--- src/main/java/com/sena/ecommerce/controller/ProductoController.java
+++ src/main/java/com/sena/ecommerce/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.sena.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,30 +20,57 @@ import com.sena.ecommerce.service.IProductoService;
 @RequestMapping("/productos")
 public class ProductoController {
 
-	//Instancia Logger
+	// Instancia Logger
 	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	private IProductoService productoservice;
-	
+
+	// metodod de listar productos
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("productos", productoservice.findALL());
 		return "productos/show";
 	}
-	
+
+	// metodo de rediccionamiemto al formulario de creacion de productos
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
-	
+
+	// Metodo de creacion de productos
 	@PostMapping
 	public String save(Producto producto) {
 		LOGGER.info("este es el objeto del producto a guardar en la DB {}", producto);
-		Usuario u = new Usuario(1,"","","","","","","","");
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "", "");
 		producto.setUsuario(u);
 		productoservice.save(producto);
 		return "redirect:/productos";
 	}
-	
+
+	// metodo para el formulario de edicion de productos
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto p = new Producto();
+		// el optional nos retorna la busqueda de un objeto de tipo producto con el id
+		Optional<Producto> op = productoservice.get(id);
+		p = op.get();
+		LOGGER.warn("Bsqueda de producto por id {}", p);
+		model.addAttribute("producto", p);
+		return "productos/edit";
+	}
+
+	// metodo de actualizacion de datos
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		LOGGER.info("este es el objeto del producto a actualizar en la DB {}", producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoservice.update(producto);
+		return "redirect:/productos";
+	}
+
+	// metodo para eliminar con id un producto
+
 }
